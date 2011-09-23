69112405b595763f6629ca0afd080347a29a5793
Sam Aaron <samaaron@gmail.com>
2011-09-23 23:54:12 +0200

    Depend on Clojure 1.3.0 final

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject overtone/at-at "0.2.0"
+(defproject overtone/at-at "0.2.1"
   :description "Ahead-of-time function scheduler"
-  :dependencies [[clojure "1.3.0-RC0"]])
+  :dependencies [[clojure "1.3.0"]])
