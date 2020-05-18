a30d261c99e292e84ba7da941560192584c3f9e9
Alexander Oloo <alekcz@gmail.com>
2020-05-18 11:11:07 +0200

    remove unneceassary change to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject overtone/at-at "1.2.0"
   :description "Ahead-of-time function scheduler"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :aot :all)
+  :dependencies [[org.clojure/clojure "1.3.0"]])
