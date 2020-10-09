312c211812868d5cf468d5a787fdaa092c2f8021
Thomas van der Veen <th.vanderveen@gmail.com>
2020-10-09 20:11:41 +0200

    fixed reflection warnins and update to latest Clojure version

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject overtone/at-at "1.2.0"
   :description "Ahead-of-time function scheduler"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]])
